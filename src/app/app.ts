65b9b01e4c7727ab8142a0614d128abfc5a8a812
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-09-08 12:02:50 +0300

    add json example for the survey

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,13 +7,13 @@ class Answer {
   public text: string;
   public weight: number;
   public parentId: number;
-  public nextQuesionId: number;
+  public nextQuestionId: number;
   constructor(json) {
     this.id = json['id'] || 0;
     this.text = json['text'] || 'no text';
     this.weight = json['weight'] || 1;
     this.parentId = json['parentId'] || 0;
-    this.nextQuesionId = json['nextQuesionId'] || 0;
+    this.nextQuestionId = json['nextQuestionId'] || 0;
   }
 }
 
@@ -22,17 +22,21 @@ class Question {
   public id: number;
   public text: string;
   public weight: number;
+  public type: string;
   public answers: Answer[];
   constructor(json) {
     this.id = json['id'] || 0;
     this.text = json['text'] || 'no text';
     this.weight = json['weight'] || 1;
-    this.answers = json['answer'] || [];
+    this.answers = json['answers'] || [];
+    this.type = json['type'] || 'textarea';
+
   }
 }
 
 
 class App {
+  public testJson: any;
   constructor() {
     console.log('App is ready');
     this.init();
@@ -41,9 +45,60 @@ class App {
   public init() {
     document.querySelector('#test').addEventListener('click', () => {
       console.log('click to test');
-    })
+    });
+    this.testJson = this.testBuilder();
+  }
+
+  testBuilder() {
+
+    const firstAnswer = new Answer({
+      id: 1,
+      text: 'first answer',
+      weight: 1,
+      parentId: 1,
+      nextQuestionId: 2
+    });
+    const secondAnswer = new Answer({
+      id: 1,
+      text: 'first answer',
+      weight: 2,
+      parentId: 1,
+      nextQuestionId: 3
+    });
+
+
+    const firstQuestion = new Question({
+      id: 1,
+      text: 'first question',
+      weight: 1,
+      answers: [firstAnswer, secondAnswer]
+    });
+
+    const secondQuestion = new Question({
+      id: 2,
+      text: 'second question',
+      weight: 1,
+      answers: []
+    });
+
+    const thirdQuestion = new Question({
+      id: 1,
+      text: 'third question',
+      weight: 1,
+      answers: []
+    });
+
+    return [
+      firstQuestion,
+      secondQuestion,
+      thirdQuestion
+    ]
   }
 
 }
 
-new App();
+const app = new App();
+
+let str = JSON.stringify(app.testJson, undefined, 4);
+console.log(str)
+document.querySelector('#jsonMonitor').innerHTML = str;
