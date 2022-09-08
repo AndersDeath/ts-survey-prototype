81b3d766901b4fe9c68184ba7085035116326597
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-09-08 14:09:42 +0300

    add filtering, bad practice not to do it this way

--- src/app/app.ts
+++ src/app/app.ts
@@ -31,12 +31,11 @@ class Question {
     this.sort = json['sort'] || 1;
     this.answers = json['answers'] || [];
     this.type = json['type'] || 'textarea';
-
   }
 }
 
 class Survey {
-  questions: Question[] = []
+  questions: Question[] = [];
   constructor(questions: Question[]){
     this.questions = questions;
   }
@@ -45,10 +44,35 @@ class Survey {
     return this.questions[0];
   }
 
-  public findById(id: number): Question {
-    return this.questions.find((e: Question) => {
-      return e.id = id;
-    })
+  public findById(id: number): Question | null {
+    for (let index = 0; index < this.questions.length; index++) {
+      if(this.questions[index].id === id) {
+        return this.questions[index];
+      }
+    }
+    return null;
+  }
+
+  public findAnswerById(answerId: number): Answer | null {
+    for (let index = 0; index < this.questions.length; index++) {
+      for (let q = 0; q < this.questions[index].answers.length; q++) {
+       if (this.questions[index].answers[q].id === answerId) {
+        return this.questions[index].answers[q];
+       }
+      }
+    }
+    return null;
+  }
+
+  public nextByAnswer(answerId: number) {
+    // console.log(questionId);
+    // console.log(answerId)
+    // const question = this.findById(questionId);
+    const answer = this.findAnswerById(answerId);
+    console.log(answer)
+    // console.log(answer.nextQuestionId);
+    // console.log(this.findById(answer.nextQuestionId))
+    return this.findById(answer.nextQuestionId);
   }
 
   public count() {
@@ -56,6 +80,53 @@ class Survey {
   }
 }
 
+
+function testBuilder(): Question[] {
+
+  const firstAnswer = new Answer({
+    id: 5,
+    text: 'first answer',
+    sort: 1,
+    parentId: 1,
+    nextQuestionId: 2
+  });
+  const secondAnswer = new Answer({
+    id: 6,
+    text: 'second answer',
+    sort: 2,
+    parentId: 1,
+    nextQuestionId: 3
+  });
+
+
+  const firstQuestion = new Question({
+    id: 1,
+    text: 'first question',
+    sort: 1,
+    answers: [firstAnswer, secondAnswer],
+    type: 'radio'
+  });
+
+  const secondQuestion = new Question({
+    id: 2,
+    text: 'second question',
+    sort: 1,
+    answers: []
+  });
+
+  const thirdQuestion = new Question({
+    id: 3,
+    text: 'third question',
+    sort: 1,
+    answers: []
+  });
+
+  return [
+    firstQuestion,
+    secondQuestion,
+    thirdQuestion
+  ]
+}
 
 class App {
   public testJson: any;
@@ -69,62 +140,20 @@ class App {
     document.querySelector('#test').addEventListener('click', () => {
       console.log('click to test');
     });
-    this.testJson = this.testBuilder();
-    this.magic(new Survey(this.testBuilder()));
+    this.testJson = testBuilder();
+    console.log(testBuilder())
+    this.magic(new Survey(testBuilder()));
   }
 
   private magic(survey: Survey) {
-    console.log(survey);
-    console.log(survey.getFirst());
-    console.log(survey.findById(3));
+    // console.log(survey);
+    // console.log(survey.getFirst());
+    // console.log(survey.findById(3));
+    const next = survey.nextByAnswer(6);
+    console.log(next);
   }
 
-  private testBuilder(): Question[] {
 
-    const firstAnswer = new Answer({
-      id: 1,
-      text: 'first answer',
-      sort: 1,
-      parentId: 1,
-      nextQuestionId: 2
-    });
-    const secondAnswer = new Answer({
-      id: 1,
-      text: 'first answer',
-      sort: 2,
-      parentId: 1,
-      nextQuestionId: 3
-    });
-
-
-    const firstQuestion = new Question({
-      id: 1,
-      text: 'first question',
-      sort: 1,
-      answers: [firstAnswer, secondAnswer],
-      type: 'radio'
-    });
-
-    const secondQuestion = new Question({
-      id: 2,
-      text: 'second question',
-      sort: 1,
-      answers: []
-    });
-
-    const thirdQuestion = new Question({
-      id: 1,
-      text: 'third question',
-      sort: 1,
-      answers: []
-    });
-
-    return [
-      firstQuestion,
-      secondQuestion,
-      thirdQuestion
-    ]
-  }
 
   getId() {
     return window.crypto.randomUUID()
