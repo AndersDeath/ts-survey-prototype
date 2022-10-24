5e70aa03aff6997dc223039fd2120ac2fdae1123
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-10-24 17:28:32 +0300

    udpate model

--- src/app/survey/survey.model.ts
+++ src/app/survey/survey.model.ts
@@ -1,3 +1,31 @@
+
+export interface AnswerModel {
+  id: number;
+  text: string;
+  sort?: number;
+  parentId: number;
+  nextId: number;
+  checked?: boolean;
+}
+
+
+export interface QuestionModel {
+  id: number;
+  text: string;
+  sort?: number;
+  answerGroup?: AnswerModel[];
+  type: string;
+  nextId: number;
+  parentId: number;
+  textAnswer?: string;
+  numberAnswer?: number;
+  base64Answer?: string;
+  scoreInterval?: number[];
+  required?: boolean;
+}
+/**
+ * Question types
+ */
 export enum QUESTION_TYPES {
   TEXTAREA = "TEXTAREA",
   RADIO = 'RADIO',
@@ -8,38 +36,102 @@ export enum QUESTION_TYPES {
   FILE_UPLOAD = 'FILE_UPLOAD'
 }
 
+/**
+ * Answer Entity
+ */
 export class Answer {
+  /**
+   * Identificator, should be unique
+   */
   public id: number;
+  /**
+   * Answer text
+   */
   public text: string;
+  /**
+   * Sort identificator
+   */
   public sort: number;
+  /**
+   * Parent question identificator
+   */
   public parentId: number;
+  /**
+   * Next question identificator
+   */
   public nextId: number;
+  /**
+   * If this answer checked or selected
+   */
   public checked: boolean;
-  constructor(json) {
-    this.id = json['id'] || 0;
-    this.text = json['text'] || 'no text';
-    this.sort = json['sort'] || 1;
+  /**
+   *  Class constructor
+   * @param json some data
+   */
+  constructor(json: AnswerModel) {
+    this.id = json.id || 0;
+    this.text = json.text || 'no text';
+    this.sort = json.sort || 1;
     this.parentId = json['parentId'] || 0;
     this.nextId = json['nextId'] || null;
     this.checked = json['checked'] || false;
   }
 }
 
+/**
+ * Question entity
+ */
 export class Question {
+  /**
+   * Question identificator, should be unique
+   */
   public id: number;
+  /**
+   * Question text, it could be html text
+   */
   public text: string;
+  /**
+   * Sort identificator
+   */
   public sort: number;
-  public type: string;
+  /**
+   * Type if question
+   */
+  public type: string; // TODO: change this type from string to QUESTION_TYPE enum
+  /**
+   * Answer group, it should be filled if we set up radio, checkbox or select question type
+   */
   public answerGroup: Answer[];
+  /**
+   * It's type of answer is filling when user answers to free text quesion, like textarea
+   */
   public textAnswer: string;
+  /**
+   * Next question identificator, it is for questions that do not have answerGroup
+   */
   public nextId: number;
+  /**
+   * Parent question identificator
+   */
   public parentId: number;
+  /**
+   *  It's type of answer is filling when user answers to score question type
+   */
   public numberAnswer: number;
+  /**
+   *  It's type of answer is filling when user uploads a photo
+   */
   public base64Answer: string;
+  /**
+   * When you set up score answer, you should set array with lower and highter scores
+   */
   public scoreInterval: number[];
+  /**
+   * is this question required or not
+   */
   public required: boolean;
 
-  constructor(json) {
+  constructor(json: QuestionModel) {
     this.id = json['id'] || 0;
     this.text = json['text'] || 'no text';
     this.sort = json['sort'] || 1;
@@ -50,7 +142,7 @@ export class Question {
       })
       return res;
     })() || [];
-    this.type = json['type'] || 'textarea';
+    this.type = json['type'] || QUESTION_TYPES.TEXTAREA;
     this.nextId = json['nextId'] || null;
     this.parentId = json['parentId'] || null;
     this.textAnswer = json['textAnswer'] || '';
@@ -61,6 +153,9 @@ export class Question {
   }
 }
 
+/**
+ * This is class for results !IMPORTANT! It is not used. It should be rewrited
+ */
 export class Result {
   text: string;
   answers: string[];
